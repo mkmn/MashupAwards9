c247e646e25c98ad260d3255c0015fd21707f262
ryotarogoto <fifty.fiftyryo0401.0613@gmail.com>
2013-10-22 12:14:12 +0000

    とりあえず整形してみました

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,4 @@
-window.onload = function(){
-  var ymap = new Y.Map("map");
-  ymap.drawMap(new Y.LatLng(35, 139), 17, Y.LayerSetId.NORMAL);
-}
+$(function(){
+var ymap = new Y.Map("map");
+ymap.drawMap(new Y.LatLng(35, 139), 17, Y.LayerSetId.NORMAL);
+});
