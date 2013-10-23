0aeb67313dcd91f9d0bc389473a182f74fc3dadc
ryotarogoto <fifty.fiftyryo0401.0613@gmail.com>
2013-10-23 01:03:17 +0000

    地図のクリック、ダブルクリック、スライドなどの動きを追加、またマーカー作成の関数も追加

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,36 @@
 $(function(){
-$('.department').select2();
-var ymap = new Y.Map("map");
-ymap.drawMap(new Y.LatLng(35, 139), 17, Y.LayerSetId.NORMAL);
+  $('.department').select2();
+  var ymap = new Y.Map("map");
+  ymap.drawMap(new Y.LatLng(35.665627,139.730738), 17, Y.LayerSetId.NORMAL);
+  ymap.setConfigure('dragging',true);
+  ymap.setConfigure('singleClickPan',true);
+  ymap.setConfigure('doubleClickZoom',true);
+  ymap.setConfigure('continuousZoom',true);
+  ymap.setConfigure('scrollWheelZoom',true);
+
+  function addMarker( n , single ) {
+    var geoBox = map.getBounds();
+    var min = geoBox.getSouthWest();
+    var max = geoBox.getNorthEast();
+    var dx = max.Lon - min.Lon;
+    var dy = max.Lat - min.Lat;
+    var markers = [];
+
+    for ( var i = 0 ; i < n ; i++ ) {
+      //ランダムなマーカーを生成
+      var lon = min.Lon + dx * Math.random();
+      var lat = min.Lat + dy * Math.random();
+      var marker = new Y.Marker( new Y.LatLng(lat,lon) );
+      if ( single ) {
+        //マーカーをひとつ追加
+        map.addFeature(marker);
+      } else {
+        markers.push( marker );
+      }
+    }
+    if ( !single ) {
+       //マーカーを複数追加
+       map.addFeatures( markers );
+    }
+  }
 });
