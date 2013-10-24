fc6eede3dc5f92ac4c255b3d07793bb550d64e2f
ryotarogoto <fifty.fiftyryo0401.0613@gmail.com>
2013-10-24 14:29:24 +0000

    top.phpを変更してみました。こんなかんじのデータ構造になるんかな?

--- src/app/top.php
+++ src/app/top.php
@@ -7,7 +7,15 @@ TOP: {
     ->name('top');
     
     // testのために記述
-    $app->post('/', function() use ($app) {
+    $app->post('/', function() use ($app, $container) {
+        $input = $app->request()->post();
+	// postされたデータから、geoコードを受け取る
+	// geoコードから酒造のデータを取得
+	$makers = '{"makers":[{"maker_name":"ＥＨ酒造",
+	  "maker_postcode":"399-8204",
+	  "maker_address":"長野県安曇野市豊科高家1090-1",
+	  "maker_url":"http://www.eh-shuzo.com/"}]
+	  }';
         $app->render('index.html.twig');
     })
     ->name('geoset');
