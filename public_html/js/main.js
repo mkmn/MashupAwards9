ac0618added6f778c6f7e215ce897bc79e7f12de
Takuya Mikami <mkmn52@gmail.com>
2013-10-28 12:13:41 +0000

    さらなる調整

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,4 @@
 $(function() {
-  $('#department').select2();
   ymap.drawMap(new Y.LatLng(35.665627,139.730738), 17, Y.LayerSetId.NORMAL);
   ymap.setConfigure('dragging',true);
   ymap.setConfigure('singleClickPan',true);
